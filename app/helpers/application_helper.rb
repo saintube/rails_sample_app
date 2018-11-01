9c23b13d30fc67bbd3bf15a48963c633c743fdd4
Frame <saintube@foxmail.com>
2018-11-01 12:57:31 +0000

    Add a full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # rendering the complete title according to the page info
+  def full_title(page_title = '')                          # define a method
+    base_title = "Ruby on Rails Tutorial Sample App"       # assign a value
+    if page_title.empty?                                   # boolean test
+      base_title                                           # return implicitly
+    else
+      page_title + " | " + base_title                      # build a string
+    end
+  end
 end
