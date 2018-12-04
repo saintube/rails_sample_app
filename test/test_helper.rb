75c8260816463cca99c750d12297672c58279044
Frame <saintube@foxmail.com>
2018-12-04 07:10:02 +0000

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,9 @@ class ActiveSupport::TestCase
   include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
+
+  # 如果用户已登录，返回true
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
