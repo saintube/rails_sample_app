c956517b15ccd9a07cb818c4629a1c1a32028625
Frame <saintube@foxmail.com>
2018-12-18 06:40:08 +0000

    Finish car edit, destroy.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,15 +57,4 @@ class UsersController < ApplicationController
 
     # 前置过滤器
 
-    # 确保是正确的用户
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
-
-    # 确保是管理员
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
-
 end
