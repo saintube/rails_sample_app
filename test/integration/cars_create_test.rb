c1425881f7dd7b48c75818dbfa65946e199bf238
Frame <saintube@foxmail.com>
2018-12-18 06:43:03 +0000

    Test version 0.12: Finish Car controller

---
+++ test/integration/cars_create_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class CarsCreateTest < ActionDispatch::IntegrationTest
+
+  test "invalid car information" do
+    log_in_as(users(:michael))
+    get new_car_path
+    assert_no_difference 'Car.count' do
+      post new_car_path, params: { car: { carname: " ", description: "xxx" } }
+    end
+    assert_template 'cars/new'
+  end
+
+  test "valid signup information" do
+    log_in_as(users(:michael))
+    get new_car_path
+    assert_difference 'Car.count', 1 do
+      post new_car_path, params: { car: { carname: "Cartest", description: "xxx" } }
+    end
+    follow_redirect!
+    assert_template 'cars/show'
+  end
+
+end
