06a980f3d9e373c3775405a0164afe8720b5b510
Frame <saintube@foxmail.com>
2018-12-19 10:53:13 +0000

    Finish Comment create action.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
     def logged_in_user
       unless logged_in?
         store_location
-        flash[:danger] = "Please log in."
+        flash[:danger] = "请先登录!"
         redirect_to login_url
       end
     end
