7e10e0e9f07f0c2c859d5219b3ef1edc65b4cc29
Frame <saintube@foxmail.com>
2019-01-01 06:44:21 +0000

    Finish captcha validation.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       log_in user
       redirect_back_or user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination or invalid captcha code'
       render 'new'
     end
   end
