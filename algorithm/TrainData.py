a5566f47a1ce8383188c221035666dd7c066712e
yeweiyang <2547611476@qq.com>
2019-01-08 13:14:55 +0800

    添加提取主题算法

---
+++ algorithm/TrainData.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   TrainData.py    
+@Contact :   raogx.vip@hotmail.com
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2019-01-08 11:44   yeweiyang      1.0         None
+'''
+
+# import lib
+from sklearn.externals import joblib
+
+def save(model, filename=None):
+    default_path = './model/train_data.pkl'
+    joblib.dump(model, filename if filename else default_path)
+
+def load(filename=None):
+    default_path = './model/train_data.pkl'
+    with open('./data/tags_token_results' + '_tag') as f:
+        return joblib.load(filename if filename else default_path), list(map(int, f.read().split('\n')[:-1]))
