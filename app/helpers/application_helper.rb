ae51831fe5978e80050ff60712c4b1eb0318c0cc
Frame <saintube@foxmail.com>
2019-01-19 08:34:37 +0000

    Fix algorithm helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+require 'open3'
+
 module ApplicationHelper
 
   # rendering the complete title according to the page info
@@ -9,4 +11,17 @@ module ApplicationHelper
       page_title + " | " + base_title                      # build a string
     end
   end
+  
+    # 返回评论的主题情感信息
+  def getThemeGrade(content, algorithm_type)
+		bcinfo = Array.new(10);
+		cmd = "python3 ../../algorithm/algorithm/FindTheme.py #{content} #{algorithm_type}";
+		Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+			while line = stdout.gets
+				bcinfo.push(line);
+			end
+		end
+		puts bcinfo;
+  end
+    
 end
